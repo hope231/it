a156f48456ae95d59751d3ba48b997650684f908
hope231 <84771862+hope231@users.noreply.github.com>
2021-06-21 19:46:55 +0500

    Add files via upload

--- js/burger.js
+++ js/burger.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$('.header-burger').click(function(event){
 		$('.header-burger,.header__menu').toggleClass('active');
-		$('body').toggleClass('lock');
+		$('body, html').toggleClass('lock');
 	});
 });
